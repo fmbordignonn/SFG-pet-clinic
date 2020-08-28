58729c7b3bf0b155a857a4975766c72f4ec196e2
unknown <felipe.bordignon@acad.pucrs.br>
2020-08-28 18:08:10 -0300

    initial commit

---
+++ src/test/java/spring/petclinic/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.petclinic.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
